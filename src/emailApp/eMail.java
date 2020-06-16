a0b563436cf8185fe0b93fe23a5f65f10d6bc167
dhalimon <Rome@206>
2020-06-16 16:58:29 -0400

    Initial commit

---
+++ src/emailApp/eMail.java
@@ -0,0 +1,26 @@
+package emailApp;
+
+public class eMail {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int   mailboxCapacity;
+    private String alternateEmail;
+
+
+    // Constructor to receive the first name and last name
+    public eMail(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAiL CREATED: " +  " " + this.lastName);
+
+    }
+    // Ask for the department
+    // Generate a random password
+    // Ser the mailbox capacity
+    // Set the alternate email
+    // Change the password
+
+
+}
---
+++ src/emailApp/emailApp.java
@@ -0,0 +1,7 @@
+package emailApp;
+
+public class emailApp {
+    public static void main(String[] args) {
+        Email em1 = new Email("John","Smith");
+    }
+}
